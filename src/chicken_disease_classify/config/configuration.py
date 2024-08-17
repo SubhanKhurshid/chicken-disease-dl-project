a75859dfcd11fc362b172367cbe3ee2477e3e338
SubhanKhurshid <k213096@nu.edu.pk>
2024-08-17 18:50:25 +0500

    done2

--- src/chicken_disease_classify/config/configuration.py
+++ src/chicken_disease_classify/config/configuration.py
@@ -1,6 +1,7 @@
 from chicken_disease_classify.constants import *
 from chicken_disease_classify.entity.config_entity import (
     DataIngestionConfig,
+    PrepareCallbacksConfig,
     PrepareModelConfig,
 )
 from chicken_disease_classify.utils.common import read_yaml, create_directories
@@ -45,3 +46,18 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories(
+            [Path(model_ckpt_dir), Path(config.tensorboard_root_log_dir)]
+        )
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath),
+        )
+
+        return prepare_callback_config
