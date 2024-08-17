58ab22d4ea4dc062bea8eae55583bcdc5eacd08f
SubhanKhurshid <k213096@nu.edu.pk>
2024-08-17 17:58:22 +0500

    init

---
+++ main.py
@@ -0,0 +1,33 @@
+from chicken_disease_classify import logger
+from chicken_disease_classify.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
+from chicken_disease_classify.pipeline.stage_02_prepare_base_model import (
+    PrepareBaseModelTrainingPipeline,
+)
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed <<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+
+
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} started <<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed <<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
